27e05321cd8e0323b6a0b337570b66fa73310c04
Anjan-Ravishankar-G <101947064+Anjan-Ravishankar-G@users.noreply.github.com>
2022-04-22 17:38:59 +0530

    change of table used

---
+++ readme for sql input.txt
@@ -0,0 +1,15 @@
+use this code to create table in your database<my database name is fox>
+
+then look at test_for_my_sql_input_file
+to understand the extract and load function in the program. 
+
+----------------------------------------------------------------------------
+CREATE TABLE testtable(
+   Names VARCHAR(255) );
+
+insert into testtable values('Anjan');   
+insert into testtable values('ravi');  
+insert into testtable values('Shankar');  
+insert into testtable values('Anjana');    
+
+------------------------------------------------------------------------------     
--- test_for_mysql_input.py
+++ test_for_mysql_input.py
@@ -2,25 +2,36 @@ import mysql.connector
 
 def load(a):
     
+    puncu="!./\{}[]!~@#$%^&*()-_<>"''
+    for ele in a:
+        if ele in puncu:
+            a=a.replace(ele,"")
+    b=a.split(',')
+    print(b)        
     mydb= mysql.connector.connect(host='localhost',user='root',passwd='12345',database='fox')
 
     mycursor = mydb.cursor()
 
-    mycursor.execute("CREATE TABLE testcase1 (rawdata varchar(1000)) ")
-    mycursor.execute("INSERT INTO testcase1 (rawdata) VALUES ({})".format(a))
-    mycursor.execute("SELECT * from testcase1")
+    # mycursor.execute("CREATE TABLE testcase1 (rawdata varchar(100)) ")
+    for i in b:
+        print(i)
+        mycursor.execute("INSERT INTO testcase1 (rawdata) VALUES (%s)",(list(i)))
+
+    mycursor.execute("select * from testcase1")
     for i in mycursor:
         print(i)
+
+    mydb.close()    
 def extract():
     mydb= mysql.connector.connect(host='localhost',user='root',passwd='12345',database='fox')
 
     mycursor = mydb.cursor()
 
-    mycursor.execute("select * from cricketers_data")
+    mycursor.execute("select * from testtable")
     a=''
     for i in mycursor:
         a=a+(str(i))
-    # print(a)
+    print(a)
     mydb.close()
     load(a)
 extract() 
